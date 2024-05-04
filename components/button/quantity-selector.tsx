765c58d4b8c19af46ece35bcc4e6161a92e86a0d
Leigh West <leighwest@hotmail.com>
2024-05-04 12:08:22 +1000

    Added animations to increment, decrement and add to cart buttons

--- components/button/quantity-selector.tsx
+++ components/button/quantity-selector.tsx
@@ -3,9 +3,9 @@ import classes from './quantity-selector.module.css';
 export default function QuantitySelector() {
   return (
     <div className={classes.wrapper}>
-      <span className={classes.clickable}>-</span>
+      <span className={`${classes.clickable} ${classes.decrement}`}>-</span>
       <span>1</span>
-      <span className={classes.clickable}>+</span>
+      <span className={`${classes.clickable} ${classes.increment}`}>+</span>
     </div>
   );
 }
